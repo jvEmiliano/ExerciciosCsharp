841a8e9341f3460ce38f7141dbe340790aaaab3e
Emiliano <emiliano202jv@gmail.com>
2023-10-02 18:23:26 -0300

    first commit

---
+++ Exercicios/0001.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ExerciciosCsharp.Exercicios
+{
+    class Exercicio0001
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using ExerciciosCsharp.Exercicios;
+
+namespace ExerciciosCsharp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+            {"0001 - Hello World", Exercicio0001.Executar}
+        });
+
+        central.SelecionarEExecutar();
+    }
+}
